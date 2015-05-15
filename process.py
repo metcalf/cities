f49ccc4431a102fcba2ad102487433c985aede3c
Andrew Metcalf <andrew@stripe.com>
2015-05-15 15:47:02 -0700

    Commit WIP when moving laptops

--- process.py
+++ process.py
@@ -3,22 +3,33 @@ import urllib
 import zipfile
 import logging
 import sys
+import csv
+import struct
+import json
+import math
+import bisect
+import base64
+from operator import attrgetter
 from cStringIO import StringIO
 from collections import namedtuple
 
 import shapefile
-# import pyproj
-# import Shapely
+from pyproj import Proj
+from shapely.geometry import shape
 
 DATA_PATH = "./data"
 URL_FORMAT = "http://www2.census.gov/geo/tiger/TIGER2010BLKPOPHU/tabblock2010_%02d_pophu.zip"
 MAX_INDEX = 57
 SKIP_INDICES = (3, 7, 14, 43, 52)
-FIELD = 'POP10'
+POP_FIELD = 'POP10'
+FIND_DISTANCE = 80000.0
+EARTH_RADIUS = 6370997.0
+MAX_CITIES = 100
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-Block = namedtuple('Block', ['lat', 'lng', 'pop', 'area'])
+Block = namedtuple('Block', ['lat', 'lon', 'pop', 'area'])
+BlockStruct = struct.Struct("iiII")
 
 
 def makeReportHook(prefix):
@@ -30,46 +41,169 @@ def makeReportHook(prefix):
     return reporthook
 
 
-def process_url(url):
+def download(url):
     filename = url.split('/')[-1]
-    basename = filename.split('.')[0]
+    filepath = os.path.join(DATA_PATH, filename)
 
-    dirpath = os.path.join(DATA_PATH, basename)
-    if not os.path.isdir(dirpath):
-        filepath = os.path.join(DATA_PATH, filename)
-
-        if not os.path.isfile(filepath):
-            prefix = 'Downloading %s' % filename
-            logging.info(prefix)
-            urllib.urlretrieve(url, filepath, makeReportHook(prefix))
-            print
-        else:
-            logging.info('Found cached %s' % filename)
-
-        os.mkdir(dirpath)
-        logging.info('Extracting to %s' % dirpath)
-        zipfile.ZipFile(filepath, 'r').extractall(dirpath)
+    if not os.path.isfile(filepath):
+        prefix = 'Downloading %s' % filename
+        logging.info(prefix)
+        urllib.urlretrieve(url, filepath, makeReportHook(prefix))
+        print
     else:
-        logging.info('Extracted data found at %s' % dirpath)
+        logging.debug('Found existing %s', filename)
 
-    sf = shapefile.Reader(os.path.join(dirpath, basename))
-    # Subtract one for the deletion flag
-    fieldIndex = [f[0] for f in sf.fields].index(FIELD) - 1
+    return filepath
 
+
+def extract_blocks(filepath):
+    basename = os.path.basename(filepath).split('.')[0]
+
+    with zipfile.ZipFile(filepath, 'r') as zipped:
+        kwargs = {}
+        for ext in ('shp', 'shx', 'dbf'):
+            fn = '%s.%s' % (basename, ext)
+            kwargs[ext] = StringIO(zipped.open(fn).read())
+
+        logging.debug('Read data into memory, processing')
+
+        sf = shapefile.Reader(**kwargs)
+        # Subtract one for the deletion flag
+        fieldIndex = [f[0] for f in sf.fields].index(POP_FIELD) - 1
+
+        for sr in sf.iterShapeRecords():
+            pop = sr.record[fieldIndex]
+
+            lons, lats = zip(*sr.shape.points)
+
+            lon = sum(lons) / len(lons)
+            lat = sum(lats) / len(lats)
+
+            pa = Proj(
+                "+proj=aea +lat_1=%.6f +lat_2=%.6f +lat_0=%.6f +lon_0=%.6f" % (
+                    min(lats), max(lats), lat, lon
+                ))
+            area = shape({
+                "type": "Polygon",
+                "coordinates": [zip(*pa(lons, lats))]
+            }).area
+
+            yield Block(lat=lat, lon=lon, pop=pop, area=area)
+
+
+def read_blockfile(filepath):
+    with open(filepath, 'r') as blockfile:
+        while True:
+            binary = blockfile.read(BlockStruct.size)
+            if binary == "":
+                break
+
+            data = BlockStruct.unpack(binary)
+
+            yield Block(
+                lon=data[0] / 1.0e6,
+                lat=data[1] / 1.0e6,
+                pop=data[2],
+                area=data[3]
+            )
+
+
+def pack_block(block):
+    area = block.area
+    if area > 4294967295:
+        logging.debug("Found huge block: %s", str(block))
+        area = 0  # Zero as sentinel for "really huge"
+
+    return BlockStruct.pack(
+        int(round(block.lon * 1e6)),
+        int(round(block.lat * 1e6)),
+        block.pop,
+        area)
+
+
+# Find all blocks that are within a dist meter square of the supplied point
+# Assumes that blocks are sorted by longitude and just naively iterates through
+# the relevant range of longitudes, checking the latitude for each.
+def find_blocks(lon, lat, dist, blocks, block_lons):
+    d_lat = math.degrees(dist /
+                         (2 * EARTH_RADIUS))
+    d_lon = math.degrees(dist /
+                         (2 * EARTH_RADIUS * math.cos(math.radians(lat))))
+
+    min_lat = lat - d_lat
+    max_lat = lat + d_lat
+    min_lon = lon - d_lon
+    max_lon = lon + d_lon
+
+    i = bisect.bisect_left(block_lons, min_lon)
+    while block_lons[i] <= max_lon:
+        block = blocks[i]
+        if block.lat >= min_lat and block.lat <= max_lat:
+            yield block
+
+        i += 1
+
+
+def process():
     blocks = []
-    for sr in sf.iterShapeRecords():
-        pop = sr.record[fieldIndex]
+    cities = []
 
-        lat = sum(p[0] for p in sr.shape.points) / len(sr.shape.points)
-        lng = sum(p[1] for p in sr.shape.points) / len(sr.shape.points)
-        area = 0
-        # area = polygon_area(shape.points)
+    for idx in range(1, MAX_INDEX):
+        if idx in SKIP_INDICES:
+            continue
+        filename = os.path.join(DATA_PATH, "%d.blk" % idx)
 
-        blocks.append(Block(lat=lat, lng=lng, pop=pop, area=area))
+        if not os.path.isfile(filename):
+            zippath = download(URL_FORMAT % idx)
 
-    return blocks
+            logging.info("Generating %s", filename)
+            # Collect in a buffer and write at once to avoid incomplete files
+            buf = StringIO()
+            for block in extract_blocks(zippath):
+                blocks.append(block)
+                buf.write(pack_block(block))
 
-for idx in range(1, MAX_INDEX):
-    if idx in SKIP_INDICES:
-        continue
-    process_url(URL_FORMAT % idx)
+            logging.debug("Writing %s", filename)
+            with open(filename, 'w') as blockfile:
+                blockfile.write(buf.getvalue())
+        else:
+            logging.info('Reading existing %s', filename)
+            for block in read_blockfile(filename):
+                blocks.append(block)
+
+    logging.debug('Got %d input blocks', len(blocks))
+
+    blocks.sort(key=attrgetter('lon'))
+    block_lons = [b.lon for b in blocks]
+
+    with open(os.path.join(DATA_PATH, 'cities.csv')) as cityfile:
+        i = 0
+        for city in csv.DictReader(cityfile):
+            if i >= MAX_CITIES:
+                break
+            i += 1
+
+            for k in ('2013-population', '2010-population'):
+                city[k] = int(city[k])
+            for k in ('area', 'lat', 'lon'):
+                city[k] = float(city[k])
+
+            # Represent as degrees East.
+            city['lon'] = -city['lon']
+
+            block_gen = find_blocks(city['lon'], city['lat'],
+                                    FIND_DISTANCE, blocks, block_lons)
+            block_data = ''.join([pack_block(b) for b in block_gen])
+            city['blocks'] = base64.b64encode(block_data)
+
+            cities.append(city)
+            logging.debug("Processed %s, %s with %d blocks",
+                          city['city'], city['state'],
+                          len(block_data) / BlockStruct.size)
+
+
+
+    json.dump(cities, open(os.path.join(DATA_PATH, 'cities.json'), 'w'))
+
+if __name__ == '__main__':
+    process()
